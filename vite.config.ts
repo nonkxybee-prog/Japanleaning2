975b9c5727efb319c07776d4727640d5db6518fc
nonkxybee-prog <nonkxybee@gmail.com>
2025-08-26 18:17:25 +0800

    Update vite config for GitHub Pages
    Update vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-/** WARNING: DON'T EDIT THIS FILE */
-/** WARNING: DON'T EDIT THIS FILE */
-/** WARNING: DON'T EDIT THIS FILE */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-function getPlugins() {
-  const plugins = [react(), tsconfigPaths()];
-  return plugins;
-}
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: getPlugins(),
-});
+  plugins: [react()],
+  base: '/Japanleaning2/', // 设置为您的仓库名称
+  build: {
+    outDir: 'dist',
+  }
+})
